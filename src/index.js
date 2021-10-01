11d8382c08c01dfbeaf5f8deb603074a3560786e
Santiago Cadavid Gutierrez <31990704+scadavidg@users.noreply.github.com>
2021-09-30 22:00:49 -0500

    Develop (#2)
    * Configuración inicial de firebase, lanzamiento de popup para seleccion correo
    
    * sesion con google + cerrar sesion

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAP_e0DeXPUpsjf32nRE8n_4hcvtchefkg",
+  authDomain: "labauth-13910.firebaseapp.com",
+  projectId: "labauth-13910",
+  storageBucket: "labauth-13910.appspot.com",
+  messagingSenderId: "834669163123",
+  appId: "1:834669163123:web:cf9683d36d600404ab3006",
+  measurementId: "G-LW6G134T48"
+}
+
+if(!firebase.apps[0]){
+  firebase.initializeApp(firebaseConfig);
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
