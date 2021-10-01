d6db87c1237e6a66d9549f615a00c268f881b8f3
scadavidg <santiago@dondo.com>
2021-09-30 21:58:40 -0500

    sesion con google + cerrar sesion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -10,6 +10,19 @@ import 'firebase/auth';
 export function App() {
 
   const provider = new firebase.auth.GoogleAuthProvider();
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user.email)
+        console.log("estas logueado")
+      } else {
+        console.log("no logueado")
+      }
+    })
+  }, [])
 
   const signInWithGoogle = () => {
     firebase.auth().signInWithPopup(provider)
@@ -23,13 +36,26 @@ export function App() {
       });
   }
 
+  const signOut = () => {
+    firebase.auth().signOut();
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           News
         </Typography>
+
+        { user ? (
+          <div>
+            {user}
+            <Button onClick={signOut} color="inherit">Logout</Button>
+          </div>
+        ) : 
+        (
         <Button onClick={signInWithGoogle} color="inherit">Login</Button>
+        )}
       </Toolbar>
     </AppBar>
   )
