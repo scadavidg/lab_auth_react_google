d5fba1c6760fec1b8a1c4da275d929b88e851728
scadavidg <santiago@dondo.com>
2021-10-01 19:25:01 -0500

    when user signOut then set user state null

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-
+import { makeStyles } from '@material-ui/core/styles';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
 export function App() {
+  const classes = useStyles();
 
   const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -20,6 +31,7 @@ export function App() {
         console.log("estas logueado")
       } else {
         console.log("no logueado")
+        setUser(null, setUser)
       }
     })
   }, [])
@@ -41,23 +53,27 @@ export function App() {
   }
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          News
-        </Typography>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Firebase Demo
+          </Typography>
+          {user ? (
+            <div>
+              {user}
+              <Button onClick={signOut} color="inherit">Logout</Button>
 
-        { user ? (
-          <div>
-            {user}
-            <Button onClick={signOut} color="inherit">Logout</Button>
-          </div>
-        ) : 
-        (
-        <Button onClick={signInWithGoogle} color="inherit">Login</Button>
-        )}
-      </Toolbar>
-    </AppBar>
+
+            </div>
+          ) :
+            (
+              <Button onClick={signInWithGoogle} color="inherit">Login</Button>
+            )}
+        </Toolbar>
+      </AppBar>
+    </div>
+
   )
 }
 
